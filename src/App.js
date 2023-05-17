acae80d3759a40c7facddbc3591099f313f0e939
Martin Halomoan Lumbangaol <martinlumbangaol880@gmail.com>
2023-05-17 10:05:39 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Box>
+			<Routes>
+				<Route path="" element={<Header />}></Route>
+			</Routes>
+		</Box>
+	);
 }
 
 export default App;

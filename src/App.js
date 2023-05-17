b72cf7a799017111b7bbbc54325e4e57fe533d3c
rarizal <110812125+rarizal@users.noreply.github.com>
2023-05-17 11:53:05 +0700

    adding redux and axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Box } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import RRegister from "./pages/rregister";
 
 function App() {
 	return (
 		<Box>
 			<Routes>
 				<Route path="/" element={<HomePage />}></Route>
+				<Route path="/register" element={<Register />}></Route>
+				<Route path="/rregister" element={<RRegister />}></Route>
 			</Routes>
 		</Box>
 	);
---
+++ src/pages/rregister.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+
+export default function RRegister() {
+	return (
+		<>
+			<Container minW={"500px"} bg={"green"} marginTop={"20px"}>
+				ffbfd
+			</Container>
+		</>
+	);
+}
